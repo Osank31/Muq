c3ce194cafa421f95a95a261cae58ba8b227bd76
Osank31 <osankverma2004@gmail.com>
2025-05-09 03:08:16 +0530

    Add hand state detection and corresponding images to GamePage

--- src/GamePage.jsx
+++ src/GamePage.jsx
@@ -4,12 +4,62 @@ import '@tensorflow/tfjs-converter';
 import '@tensorflow/tfjs-backend-webgl';
 import * as handpose from '@tensorflow-models/handpose';
 import * as handPoseDetection from '@tensorflow-models/hand-pose-detection';
+import OpenHandImage from '../public/open_hand-removebg-preview.png'
+import ClosedHandImage from '../public/close_hand-removebg-preview.png'
 
 const GamePage = () => {
 
     const videoRef = useRef(null);
     const canvasRef = useRef(null);
     const modelRef = useRef(null);
+    const HandRef = useRef(null);
+
+    function getHandedness(handedness) {
+        if (handedness === 'Left')
+            return 'Right'
+        return 'Left'
+    }
+
+    function calculateDistance(coordinates1, coordinates2) {
+        let [x1, y1] = [coordinates1.x, coordinates1.y];
+        let [x2, y2] = [coordinates2.x, coordinates2.y];
+
+        return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    }
+
+    function getHandState(keypoints, handedness) {
+        let answer = 'Open';
+        let [thumb, indexFinger, ringFinger, middleFinger, littleFinger] = [false, false, false, false, false];
+
+        if (keypoints[8].y > keypoints[5].y) {
+            indexFinger = true;
+        }
+        if (keypoints[12].y > keypoints[9].y) {
+            middleFinger = true;
+        }
+        if (keypoints[16].y > keypoints[13].y) {
+            ringFinger = true;
+        }
+        if (keypoints[20].y > keypoints[17].y) {
+            littleFinger = true;
+        }
+
+        if (handedness === "Right") {
+            if (keypoints[4].x < keypoints[2].x) {
+                thumb = true;
+            }
+        } else if (handedness === "Left") {
+            if (keypoints[4].x > keypoints[2].x) {
+                thumb = true;
+            }
+        }
+
+        if (indexFinger && middleFinger && ringFinger && littleFinger && thumb) {
+            answer = "Closed";
+        }
+
+        return answer;
+    }
 
     useEffect(() => {
         const setupCamera = async () => {
@@ -48,23 +98,33 @@ const GamePage = () => {
 
             const detect = async () => {
                 if (modelRef.current && videoRef.current && videoRef.current.readyState === 4) {
-                    const predictions = await modelRef.current.estimateHands(videoRef.current, {
-                        // flipHorizontal: true
-                    });
+                    const predictions = await modelRef.current.estimateHands(videoRef.current);
                     ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
                     ctx.save();
                     ctx.scale(-1, 1);
                     ctx.translate(-canvasRef.current.width, 0);
                     ctx.drawImage(videoRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height);
                     if (predictions.length > 0) {
-                        console.log((predictions));
-                        predictions[0].keypoints.forEach(coordinates => {
-                            
-                            const [x,y] = [coordinates.x, coordinates.y]
 
+                        predictions[0].handedness = getHandedness(predictions[0].handedness);
+                        predictions[0].handState = getHandState(predictions[0].keypoints, predictions[0].handedness);
+                        // console.log((predictions[0]));
+
+                        
+                        if (HandRef.current) {
+                            if (predictions[0].handState === 'Open')
+                                HandRef.current.src = OpenHandImage;
+                            if (predictions[0].handState === 'Closed')
+                                HandRef.current.src = ClosedHandImage;
+                        }
+                        
+                        console.log(HandRef.current.src)
+
+                        predictions[0].keypoints.forEach(coordinates => {
+                            const [x, y] = [coordinates.x, coordinates.y]
                             ctx.beginPath();
-                            ctx.arc(x,y,5,0,2*Math.PI);
-                            ctx.fillStyle='red';
+                            ctx.arc(x, y, 5, 0, 2 * Math.PI);
+                            ctx.fillStyle = 'red';
                             ctx.fill();
                         });
                     }
@@ -93,8 +153,9 @@ const GamePage = () => {
                 ref={canvasRef}
                 width={640}
                 height={480}
-                style={{ position: 'absolute', top: 0, left: 0 }}
+                style={{visibility: 'hidden'}}
             />
+            <img ref={HandRef}/>
         </div>
     )
 }
